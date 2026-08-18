2f13d37428a3adaaa2b962f3c2abffeb59276a9c
Md Imran Shakeel <imshkl2005@gmail.com>
2026-08-18 19:34:51 +0530

    Update longestsequence04.cpp

--- longestsequence04.cpp
+++ longestsequence04.cpp
@@ -1,49 +1,32 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-using namespace std;
-// Function to find the length of the longest consecutive sequence
-int longestConsecutive(vector<int>& nums)
-{
-    unordered_map<int, bool> mp;
-    for (int num : nums)
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int,bool>mp;
+        int longest=0;
+        for (int num : nums)
     {
         mp[num] = true;
     }
-    // Stores the maximum length of any consecutive sequence
-    int longest = 0;
-    // Traverse every element in the array
-    for (int num : nums)
-    {
-        //"If num - 1 does not exist in the HashMap."
-        if (mp.find(num - 1) == mp.end())
-        {// Start counting the sequence from 'num'
-            int current = num;
-            int length = 1;
-            // Continue while the next consecutive number exists
-            while (mp.find(current + 1) != mp.end())
-            {
-                current++;   // Move to next consecutive number
-                length++;    // Increase sequence length
+        for(auto it:mp){
+            int num=it.first;
+            if(mp.find(num-1)==mp.end() ){
+              int curr=num;
+              int length=1;
+             // mp[num]=false;
+              while(mp.find(curr+1)!=mp.end()){
+                
+                curr++;
+                length++;
+              }
+            
+
+              longest=max(longest,length);
             }
-            // Update the maximum sequence length found so far
-            longest = max(longest, length);
+           
+           
         }
+        return longest;
+
+        
     }
-    // Return the longest consecutive sequence length
-    return longest;
-}
-int main()
-{
-    vector<int> nums = {100, 4, 200, 1, 3, 2};
-
-    cout << "Longest Consecutive Sequence Length = "
-         << longestConsecutive(nums);
-
-    return 0;
-}
-
-// It asks:
-// "Does the previous number exist?"
-// If yes → this number is somewhere in the middle → skip it
-// If no → this number is the start → count forward
+};
